a9f36cf8b778481f8c0684855f14f51446cf6c4c
kareem sakr <k.sakr90@gmail.com>
2017-07-15 14:33:06 +0400

    added a header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <div>
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link className="navbar-brand" to="/">Artzavaganza</Link>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/posts">Art Posts</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/artists">Artists</Link>
+              </li>
+            </ul>
+            
+          </div>
+        </nav>
+      </div>
     );
   }
 }
